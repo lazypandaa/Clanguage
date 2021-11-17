21488cae91b50bb75a253f4f1de3bc1e9c088146
Eswar_lazypanda <72182474+lazypandaa@users.noreply.github.com>
2021-11-17 20:55:20 +0530

    Addition of two number

---
+++ [2]add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h> 
+
+void main()
+{
+    int a,b,c;
+    clrscr();
+    printf("Addition of two numbers");
+    a= 12;
+    b= 52;
+    c= a+b;
+    printf("%d",c);
+    getch();
+}
