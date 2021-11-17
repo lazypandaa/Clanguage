bf02ba8436c4051c5529a620245e734436478630
Eswar_lazypanda <72182474+lazypandaa@users.noreply.github.com>
2021-11-17 21:36:39 +0530

    We have to assume the value

---
+++ assume.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    short int a=127,b=128,c=129;
+    unsigned short int a1=255,b1=256,c1=257;
+    clrscr();
+    printf("a=%d\t b=%d\t c=%d\n", a,b,c);
+    printf("a1=%u\t b1=%u\t c1=%ud\t", a1,b1,c1);
+    getch();
+}
