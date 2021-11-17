9f220d88b0fea6336fadb3f930ec47c0f165ed5c
Eswar_lazypanda <72182474+lazypandaa@users.noreply.github.com>
2021-11-17 21:24:02 +0530

    Finding the size of datatypes

---
+++ 10.size of datatypes.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    // clrscr();
+    printf("Size of(short int)=%ld\n", sizeof (short int));
+    printf("Size of (int)=%ld\n",sizeof (int));
+    printf("Size of (long int)=%ld\n", sizeof (long int));
+    printf("Size of float)=%ld\n", sizeof (float));
+    printf("Size of (Double)=%ld\n", sizeof (double));
+    printf("size of (long double)=%ld\n", sizeof (long double));
+    printf("Size of (char)=%ld\n", sizeof (char));
+    // getch();
+    return 0;
+}
