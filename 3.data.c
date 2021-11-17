ccf8675203a46c819de86303bf944c6255103247
Eswar_lazypanda <72182474+lazypandaa@users.noreply.github.com>
2021-11-17 21:06:56 +0530

    c program using all three datatypes

---
+++ 3.data.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<conio.h> 
+
+void main()
+{
+    int a=10;
+    float f= 15.258;
+    char ch='J';
+    printf("Write a c program using all three datatypes\n");
+    // clrscr();
+    printf("Single line\n");
+    printf("a=%d, f=%f, ch=%c\n",a,f,ch);
+    printf("Multie line using \\n");
+    printf("\na=%d\nf=%f\nch=%c\n",a,f,ch);
+    // getch();
+}
