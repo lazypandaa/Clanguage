84a2d3a7318e73fee3f1ad8be2a0b3d605852733
Eswar_lazypanda <72182474+lazypandaa@users.noreply.github.com>
2021-11-18 20:10:20 +0530

    arithmetic operations

---
+++ arthematic.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+//arthematic operations
+void main()
+
+{
+    int a,b,c;
+    // clrscr();
+    printf("Enter a,b Values: ");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("\na+b=%d\n",c);
+    c=a-b;
+    printf("a-b=%d\n",c);
+    c=a*b;
+    printf("a*b=%d\n",c);
+    c=a/b;
+    printf("a/b=%d\n",c);
+    c=a%b;
+    printf("a mod b=%d",c);
+    // getch();
+
+}
