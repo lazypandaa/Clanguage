07f2cc8b471af1e254a5a67c0e909cdbf2d3690b
Eswar_lazypanda <72182474+lazypandaa@users.noreply.github.com>
2021-11-18 20:37:53 +0530

    Ascii, Octal, Hexadecimal values in c
    ASCII
    a character variable holds an ASCII value (an integer number between 0 and 127) rather than that character itself. The ASCII value of the lowercase alphabet is from 97 to 122. And, the ASCII value of the uppercase alphabet is from 65 to 90.

---
+++ asci_octal_hexa.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+
+{
+    char ch ='A';
+    // clrscr();
+    printf("%c=%d\t%o\t%x",ch,ch,ch,ch);
+    char chh ='b';
+    printf("\n%c=%d\t%o\t%x",chh,chh);
+    // getch();
+
+}
