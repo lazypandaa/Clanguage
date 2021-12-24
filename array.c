ad720b4b297c5a12c1c15ad2ec3b6b65056f7a7c
Eswar_lazypanda <72182474+lazypandaa@users.noreply.github.com>
2021-12-24 10:11:39 +0530

    string char
    C program ot illustrate string char

---
+++ array.c
@@ -0,0 +1,10 @@
+// 26. Write a c program ot illustrate string char
+#include <stdio.h>
+
+int main()
+{
+    char addrs[70];
+    scanf("%[a-z, ]",addrs);
+    printf("Address: %s",addrs);
+    return 0;
+}
