0fc15726a9d5becb2bf9bbbb66d4e80f5ad20f6d
Eswar_lazypanda <72182474+lazypandaa@users.noreply.github.com>
2022-02-24 19:15:39 +0530

    Write a c program to check the biggest of 3 numbers using "Nested-if"?

---
+++ nbiggestof3.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter a Number: ");
+    scanf("%d%d%d",&a,&b,&c);
+    if (a>b)
+        if (a>c)
+            printf("%d is bigger",a);
+        else
+            printf("%d is bigger",c);
+    else
+        if (b>c)
+            printf("%d id bigger",b);
+        else
+            printf("%d is bigger",c);
+    return 0;
+}
